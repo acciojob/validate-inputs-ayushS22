f1ca861251541ddcdf9b7f8307f8cd6b90f26f67
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2026-01-20 20:07:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+function validateName() {
+  const name = document.getElementById("name");
+  const pattern = /^[A-Za-z]{3,}$/;
+
+  name.style.backgroundColor = pattern.test(name.value)
+    ? "lightgreen"
+    : "pink";
+}
+
+function validateEmail() {
+  const email = document.getElementById("email");
+  const pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+  email.style.backgroundColor = pattern.test(email.value)
+    ? "lightgreen"
+    : "pink";
+}
+
+function validateMobile() {
+  const mobile = document.getElementById("mobile");
+  const pattern = /^[0-9]{10}$/;
+
+  mobile.style.backgroundColor = pattern.test(mobile.value)
+    ? "lightgreen"
+    : "pink";
+}
